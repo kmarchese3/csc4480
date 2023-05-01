aeb29d78e4fb596e9e133d857673ae55da6bbca8
NBevins <nab109580@gmail.com>
2023-05-01 19:44:18 -0400

    Added queries

---
+++ test-queries.sql
@@ -0,0 +1,19 @@
+-- Nathaniel Bevins
+-- Select the team whose head coach is John Smith
+SELECT team_name 
+FROM coaches c
+WHERE c.coaches_name = 'John Smith';
+
+-- Nathaniel Bevins
+-- Find the 3rd Baseman for the team coached by Vana White
+SELECT player_number, player_name, p.position
+FROM coaches c, players p
+WHERE c.coaches_name = 'Vana White' AND c.team_name = p.team_name;
+
+-- Nathaniel Bevins
+-- Find all the games played in by Babe Ruth
+SELECT g.start_time, g.game_date, g.home_team, g.away_team
+FROM games g, players p, team_plays tp
+WHERE p.player_name = 'Babe Ruth' AND p.team_name = tp.team_name 
+    AND tp.game_start = g.start_time AND tp.game_loc = g.field_loc 
+    AND tp.game_field = g.field_num AND tp.game_date = g.game_date;
